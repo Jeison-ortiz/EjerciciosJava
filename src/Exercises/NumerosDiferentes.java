23fc0adc6336f11b69c1b466852758d2a97ffd48
Jeison Ortiz <ortizjeison4@gmail.com>
2023-03-16 22:04:23 -0500

    feat(numeros diferentes): Se saca de una lista los numeros diferentes, se elimina los repetidos.

---
+++ src/Exercises/NumerosDiferentes.java
@@ -0,0 +1,32 @@
+package Exercises;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NumerosDiferentes {
+
+//	public static void main(String[] args) {
+//	List<Integer> myList = new ArrayList<>();
+//	myList.add(5);
+//	myList.add(5);
+//	myList.add(5);
+//	myList.add(5);
+//	myList.add(2);
+//	myList.add(3);
+////	int rta = cantidadMinimaDeDias(myList);
+//	
+//
+//	public static int cantidadMinimaDeDias(List<Integer> x) {
+//		Set<Integer> miSet = new HashSet<Integer>();
+//		for (Integer number : x) {
+//			miSet.add(number);
+//		}
+//		System.out.println(miSet.size());
+//		return miSet.size();
+//
+//	}
+//	}
+
+}
